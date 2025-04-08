8993415ae4567a3e64fbb8353328023060982f4d
Moazzam-15 <DanishHassan.Malik1@my-aolcc.com>
2025-04-08 14:17:58 +0500

    portfoilio website

---
+++ script.js
@@ -0,0 +1,5 @@
+const contactButton = document.getElementById('contactButton');
+
+contactButton.addEventListener('click', function () {
+    alert('Thanks for reaching out! I will reply to you soon.');
+});
